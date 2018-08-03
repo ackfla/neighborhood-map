3e6d0efa881cbac9a97769c93556e629557126ff
Lucy Weatherley <lucyweath@msn.com>
2018-08-03 19:06:43 +0100

    Search bar added to menu

--- src/Menu.js
+++ src/Menu.js
@@ -2,9 +2,25 @@ import React, { Component } from 'react';
 
 class Menu extends Component {
 
+  state = {
+    query: ''
+  }
+
+  updateQuery = (event) => {
+    this.setState({
+      query: event.target.value
+    })
+  }
+
   render() {
     return (
       <nav>
+        <input
+          type='text'
+          placeholder='Filter by location'
+          value={this.state.query}
+          onChange={this.updateQuery}
+        />
         <ul>
           {this.props.locations.map((location) => (
             <li key={location.title}>
